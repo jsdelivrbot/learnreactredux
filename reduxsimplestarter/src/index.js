66d52c48c6e6f955eb184f2a2e5783f968719393
Scott Sargent <scott.sargent@gmail.com>
2017-01-07 14:42:37 -0500

    Wiring up video player, video list and video list item

--- reduxsimplestarter/src/index.js
+++ reduxsimplestarter/src/index.js
@@ -1,15 +1,45 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
+
 import * as keys from './apiKeys';
 import SearchBar from './components/search_bar'
+import VideoList from './components/video_list'
+import VideoDetail from './components/video_detail'
 
-// Create a new component
-// some html
-const App = function(){
-  return <div>
-    <SearchBar />
-  </div>;
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      videos: [],
+      selectedVideo: null
+    };
+
+    this.videoSearch('Mesa Stiletto');
+
+  }
+
+  videoSearch(term) {
+    YTSearch({key: keys.YouTubeApiKey, term: term}, videos => {
+      this.setState({
+        videos:videos,
+        selectedVideo: videos[0]
+       });
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <SearchBar onSearchTermChange={term => this.videoSearch(term)} />
+        <VideoDetail video={this.state.selectedVideo} />
+        <VideoList
+          onVideoSelect={selectedVideo => this.setState({selectedVideo})}
+          videos={this.state.videos} />
+      </div>
+    );
+  }
 }
-// take this components generated html and put it
-//on the page (in the dom)
+
 ReactDOM.render(<App />, document.querySelector('.container'));
