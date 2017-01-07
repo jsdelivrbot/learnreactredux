f4a3288360336c65a09028d824f5ea71ea1af3b0
Scott Sargent <scott.sargent@gmail.com>
2017-01-07 13:06:18 -0500

    Creating search_bar from udemy react video

--- reduxsimplestarter/src/index.js
+++ reduxsimplestarter/src/index.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import * as keys from './apiKeys';
+import SearchBar from './components/search_bar'
+
 // Create a new component
 // some html
 const App = function(){
-  return <div>Hi!</div>;
+  return <div>
+    <SearchBar />
+  </div>;
 }
 // take this components generated html and put it
 //on the page (in the dom)
