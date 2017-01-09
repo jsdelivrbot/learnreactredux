eaaa155cc45e24f36e28e14bc2df22bb8c626d98
Scott Sargent <ssargent@paylocity.com>
2017-01-09 12:13:01 -0500

    Adding book list app from course

---
+++ book_list/src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function() {
+  return [
+    {title: 'Javscript: The Good Parts'},
+    {title: 'Harry Potter'},
+    {title: 'Fearless: The Undaunted Courage and Ultimate Sacrifice of Navy SEAL Team SIX Operator Adam Brown'},
+    {title: 'The Lord of the Rings'}
+  ];
+}
