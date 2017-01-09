f222f0602ae6d7b1e46c502476725fb2872edd4b
Scott Sargent <ssargent@paylocity.com>
2017-01-09 13:04:47 -0500

    Adding actions and more reducers

--- book_list/src/containers/book-list.js
+++ book_list/src/containers/book-list.js
@@ -1,11 +1,16 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { selectBook } from '../actions/index';
+import { bindActionCreators } from 'redux';
 
 class BookList extends Component {
   renderList() {
     return this.props.books.map((book) => {
       return (
-        <li key={book.title} className="list-group-item">{book.title}</li>
+        <li
+          key={book.title}
+          onClick={() => this.props.selectBook(book)} 
+          className="list-group-item">{book.title}</li>
       );
     });
   }
@@ -25,5 +30,15 @@ function mapStateToProps(state) {
     };
 }
 
-export default connect(mapStateToProps)(BookList);
- 
+// anything returned from this function will end up as props
+// on the BookList container.
+function mapDispatchToProps(dispatch) {
+  // whenever selectbook is called, the result should be passed
+  // to all of our reducers.
+  return bindActionCreators({selectBook: selectBook}, dispatch);
+}
+
+// promote booklist from component to container
+// it needs to know about this new dispatch method, selectBook
+// as a prop.
+export default connect(mapStateToProps, mapDispatchToProps)(BookList);
--- book_list/src/reducers/reducer_books.js
+++ book_list/src/reducers/reducer_books.js
@@ -1,8 +1,8 @@
 export default function() {
   return [
-    {title: 'Javscript: The Good Parts'},
-    {title: 'Harry Potter'},
-    {title: 'Fearless: The Undaunted Courage and Ultimate Sacrifice of Navy SEAL Team SIX Operator Adam Brown'},
-    {title: 'The Lord of the Rings'}
+    {title: 'Javscript: The Good Parts', pages: 176},
+    {title: 'Harry Potter and the Sorcer\'s Stone', pages: 309},
+    {title: 'Fearless: The Undaunted Courage and Ultimate Sacrifice of Navy SEAL Team SIX Operator Adam Brown', pages: 368},
+    {title: 'The Lord of the Rings', pages: 1178}
   ];
 }
