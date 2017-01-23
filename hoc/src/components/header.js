5680b488cee5e052aacbbaebd59446c1136a78fb
Scott Sargent <ssargent@paylocity.com>
2017-01-23 15:50:26 -0500

    Updating Testing and creating hoc

---
+++ hoc/src/components/header.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { Link } from 'react-router';
+
+export default class Header extends React.Component {
+  authButton() {
+    return (<button className="btn btn-primary">Sign In</button>);
+  }
+
+  render() {
+    return (
+      <nav className="navbar navbar-light">
+        <ul className="nav navbar-nav">
+          <li className="nav-item">
+            <Link to="/">Home</Link>
+          </li>
+          <li className="nav-item">
+            <Link to="/resources">Resources</Link>
+          </li>
+          <li className="nav-item">
+            { this.authButton()}
+          </li>
+        </ul>
+      </nav>
+    );
+  }
+}
---
+++ hoc/src/components/resources.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default () => {
+  return (
+    <div>
+      <strong>B</strong>allistic <strong>W</strong>ater <strong>B</strong>alloon <strong>L</strong>aunch <strong>S</strong>ystem Keys: 8ED8DAC0-DE23-4767-8078-46D552397C1B 5A5C65C3-FE63-4C08-8627-7B566A09F96C
+    </div>
+  );
+};
